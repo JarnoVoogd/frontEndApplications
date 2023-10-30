9ca7f90141ab617c92b034e8abc46ce9ef08f434
Jarno Voogd <jarnovoogd@Jarnos-MacBook-Pro.local>
2023-10-30 22:20:27 +0100

    created and imported components, first working request to SWAPI with returned data

--- fea-svelte-app/src/components/ProcessData.svelte
+++ fea-svelte-app/src/components/ProcessData.svelte
@@ -9,8 +9,17 @@
     // Process the data and populate somPerSoort
     personenData.forEach(i => {
       somPerSoort[i["eye_color"]] = (somPerSoort[i["eye_color"]] || 0) + 1;
-    }
+    })
   }
 </script>
+
+<h1>Personen</h1>
+{#each personenData as persoon}
+  <p>{persoon.name}</p>
+{/each}
+<h2>Overzicht per oogkleur</h2>
+{#each Object.keys(somPerSoort) as oogkleur}
+  <p>{oogkleur}: {somPerSoort[oogkleur]}</p>
+{/each}
 
 <FetchData on:dataLoaded={handleDataLoaded} />
--- fea-svelte-app/src/components/fetchData.svelte
+++ fea-svelte-app/src/components/fetchData.svelte
@@ -1,43 +1,40 @@
-let allFetches = [];
-let somPerSoort = [];
-let personenData = [];
+<script>
+  import { onMount } from "svelte";
+  import { createEventDispatcher } from "svelte";
 
-function fetchPeople(id) {
+  const dispatch = createEventDispatcher();
+
+  let allFetches = [];
+  let endFetch = 0;
+
+  function fetchPeople(id) {
   const url = `https://swapi.dev/api/people/${id}/`;
   return fetch(url)
     .then(response => {
       if (response.status === 200) {
         return response.json();
       } else {
-        return null; // Return null for non-200 responses
+        return null;
       }
     });
 }
 
-let endFetch = 0;
-
-for (let i = 0; endFetch < 3 && i < 20; i++) {
-  allFetches.push(fetchPeople(i).then(persoon => {
-    if (persoon !== null) {
-      return persoon; // Return the fetched data
-    } else {
-      endFetch++;
-      return null; // Return null for error cases
+  onMount(() => {
+    for (let i = 1; endFetch < 3 && i < 20; i++) {
+      allFetches.push(fetchPeople(i).then(persoon => {
+        if (persoon !== null) {
+          return persoon; // Return the fetched data
+        } else {
+          endFetch++;
+          return null; // Return null for error cases
+        }
+      }));
     }
-  }));
-}
 
-Promise.all(allFetches).then(allePersonen => {
-  allePersonen = allePersonen.filter(persoon => persoon !== null); // Remove null values
-
-  for (let i = 0; i < allePersonen.length; i++) {
-    personenData.push(allePersonen[i]["eye_color"]);
-  }
-  console.log(personenData);
-
-  personenData.forEach((i) => {
-    somPerSoort[i] = (somPerSoort[i] || 0) + 1;
+    // Fetch data and emit an event when done
+    Promise.all(allFetches).then(allePersonen => {
+      allePersonen = allePersonen.filter(persoon => persoon !== null);
+      dispatch("dataLoaded", allePersonen);
+    });
   });
-
-  console.log(somPerSoort);
-});
+</script>
