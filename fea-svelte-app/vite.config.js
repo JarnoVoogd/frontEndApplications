8695a57c01231b255d5708f15edbba9115867fdf
Jarno Voogd <jarnovoogd@Jarnos-MacBook-Pro.local>
2023-10-30 22:19:34 +0100

    created proxy because of CORS errors when requesting data from SWAPI

--- fea-svelte-app/vite.config.js
+++ fea-svelte-app/vite.config.js
@@ -2,5 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+		  // Create a proxy for SWAPI
+		  '/https://swapi.dev': {
+			target: 'https://swapi.dev',
+			changeOrigin: true,
+			rewrite: (path) => path.replace(/^\/https:\/\/swapi.dev/, ''),
+		  },
+		},
+	  },
 });
