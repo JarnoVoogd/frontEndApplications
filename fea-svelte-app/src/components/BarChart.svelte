4b4ead1ba4f73565a43b6fae4f4a651d46fd6d28
Jarno Voogd <jarnovoogd@gmail.com>
2023-11-30 00:50:52 +0100

    made the tooltip I created in PieChart.svelte into a component and made it work with both the bar and piechart

--- fea-svelte-app/src/components/BarChart.svelte
+++ fea-svelte-app/src/components/BarChart.svelte
@@ -1,80 +1,74 @@
+<!-- BarChart.svelte -->
+
 <script>
   import { onMount, afterUpdate } from 'svelte';
   import * as d3 from 'd3';
   import { eyeColorScale } from './EyeColorScale.js';
+  import Tooltip from './Tooltip.svelte';
 
   export let data;
 
-  // Variables to store the SVG container, scales, and chart
   let svg;
   let x, y, chart;
+  let tooltip;
 
-  // Lifecycle hook: runs after the component is first mounted to the DOM
   onMount(() => {
-    // Select the SVG container by ID
     svg = d3.select('#bar-chart');
-    // Create linear and band scales for x and y axes
     x = d3.scaleLinear();
     y = d3.scaleBand().padding(0.1);
-    // Create a group element within the SVG to hold the chart
     chart = svg.append('g');
 
-    // Initialize the bar chart
+    // Instantiate the Tooltip component
+    tooltip = new Tooltip({
+      target: document.body,
+      props: {
+        content: '',
+        left: 0,
+        top: 0,
+        opacity: 0
+      }
+    });
+
     createBarChart();
   });
 
-  // Lifecycle hook: runs after a component updates
   afterUpdate(() => {
-    // Update the bar chart if the data changes
     updateBarChart();
   });
 
-  // Function to create the initial bar chart
   function createBarChart() {
-    // Define margins and dimensions for the chart
     const margin = { top: 20, right: 20, bottom: 30, left: 40 };
     const width = 600 - margin.left - margin.right;
     const height = 400 - margin.top - margin.bottom;
 
-    // Set the ranges for the x and y scales
     x.range([0, width]);
     y.range([0, height]);
 
-    // Set the transform attribute for the chart group element
     chart.attr('transform', `translate(${margin.left},${margin.top})`);
 
-    // Append y-axis group element to the chart
     chart
       .append('g')
       .attr('class', 'y-axis')
-      .call(d3.axisLeft(y)); // Use axisLeft for y-axis
+      .call(d3.axisLeft(y));
 
-    // Append x-axis group element to the chart and translate it to the bottom
     chart.append('g').attr('class', 'x-axis').attr('transform', `translate(0, ${height})`);
   }
 
-  // Function to update the bar chart when the data changes
   function updateBarChart() {
-    // Define margins and dimensions for the chart
     const margin = { top: 20, right: 20, bottom: 30, left: 40 };
     const width = 600 - margin.left - margin.right;
     const height = 400 - margin.top - margin.bottom;
 
-    // Set the ranges for the x and y scales
     x.range([0, width]);
     y.range([0, height]);
 
-    // Set the transform attribute for the chart group element
     chart.attr('transform', `translate(${margin.left},${margin.top})`);
 
-    // Adjust the domain of the x and y scales based on the data
     x.domain([0, d3.max(data, (d) => d.value)]);
     y.domain(data.map((d) => d.key));
 
-    // Remove existing bars
     chart.selectAll('.bar').remove();
 
-    // Add new bars with colors based on eyeColorScale mapping
     chart
       .selectAll('.bar')
       .data(data)
@@ -85,11 +79,31 @@
       .attr('y', (d) => y(d.key))
       .attr('width', (d) => x(d.value))
       .attr('height', y.bandwidth())
-      .attr('fill', (d) => eyeColorScale[d.key]);
+      .attr('fill', (d) => eyeColorScale[d.key])
+      .on('mouseover', handleMouseOver)
+      .on('mouseout', handleMouseOut);
 
-    // Update the y-axis and x-axis based on the updated scales
     chart.select('.y-axis').call(d3.axisLeft(y));
-    chart.select('.x-axis').call(d3.axisBottom(x)); // Use axisBottom for x-axis
+    chart.select('.x-axis').call(d3.axisBottom(x));
+  }
+
+  function handleMouseOver(event, d) {
+    const percentage = ((d.value / d3.sum(data, (d) => d.value)) * 100).toFixed(2);
+
+    // Update the Tooltip component's props using set()
+    tooltip.$set({
+      content: `<strong>Eye Color:</strong> ${d.key}<br/>` +
+               `<strong>Amount:</strong> ${d.value}<br/>` +
+               `<strong>Percentage:</strong> ${percentage}%`,
+      left: event.pageX + 'px',
+      top: event.pageY - 28 + 'px',
+      opacity: 1
+    });
+  }
+
+  function handleMouseOut() {
+    // Update the Tooltip component's opacity using set()
+    tooltip.$set({ opacity: 0 });
   }
 </script>
 
@@ -98,4 +112,3 @@
 </style>
 
 <svg id="bar-chart" width="600" height="400"></svg>
-
--- fea-svelte-app/src/components/PieChart.svelte
+++ fea-svelte-app/src/components/PieChart.svelte
@@ -1,117 +1,96 @@
-<!-- piechart.svelte -->
+<!-- PieChart.svelte -->
 
 <script>
   import { onMount, afterUpdate } from 'svelte';
   import * as d3 from 'd3';
   import { eyeColorScale } from './EyeColorScale.js';
+  import Tooltip from './Tooltip.svelte';
 
   export let data;
 
-  // Variables to store the SVG container, radius, pie function, and tooltip
   let svg;
   let radius;
   let pie;
   let tooltip;
 
-  // Store the current data outside the component
   let currentData;
 
-  // Lifecycle hook: runs after the component is first mounted to the DOM
   onMount(() => {
-    // Select the SVG container by ID
     svg = d3.select('#pie-chart');
-    // Calculate the radius based on the minimum of width and height divided by 2
-    radius = Math.min(300, 300) / 2; // Adjust the radius as needed
-    // Create a pie function that calculates angles based on the data values
+    radius = Math.min(300, 300) / 2;
     pie = d3.pie().value((d) => d.value);
 
-    // Initialize the pie chart
     createPieChart();
   });
 
-  // Lifecycle hook: runs after a component updates
   afterUpdate(() => {
-    // Update the pie chart if the data changes
-    updatePieChart();
+    if (JSON.stringify(currentData) !== JSON.stringify(data)) {
+      updatePieChart();
+    }
   });
 
-  // Function to create the initial pie chart
   function createPieChart() {
-    // Set the width and height of the SVG container
     svg.attr('width', 300).attr('height', 300);
 
-    // Create a tooltip and append it to the body
-    tooltip = d3.select('body').append('div').attr('class', 'tooltip');
+    tooltip = new Tooltip({
+      target: document.body,
+      props: { content: '' },
+    });
 
-    // Create a group element within the SVG and position it at the center
     svg
-    .append('g')
+      .append('g')
       .attr('transform', `translate(${radius},${radius})`)
       .selectAll('path')
       .data(pie(data))
       .enter()
       .append('path')
       .attr('class', 'arc')
-      // Use the eyeColorScale mapping to determine the fill color
       .attr('fill', (d) => eyeColorScale[d.data.key])
       .on('mouseover', handleMouseOver)
       .on('mouseout', handleMouseOut);
   }
 
-  // Function to update the pie chart when the data changes
   function updatePieChart() {
-    // Remove the existing paths
     svg.selectAll('.arc').remove();
 
-    // Create a group element within the SVG and position it at the center
     const arcs = svg
-    .append('g')
+      .append('g')
       .attr('transform', `translate(${radius},${radius})`)
       .selectAll('path')
       .data(pie(data))
       .enter()
       .append('path')
       .attr('class', 'arc')
-      // Use the eyeColorScale mapping to determine the fill color
       .attr('fill', (d) => eyeColorScale[d.data.key])
       .on('mouseover', handleMouseOver)
       .on('mouseout', handleMouseOut);
-      
-    // Calculate start and end angles for each data point
+
     const arcTween = (d) => {
       const i = d3.interpolate(currentData, d);
       currentData = i(0);
       return (t) => d3.arc().innerRadius(0).outerRadius(radius)(i(t));
     };
 
-    // Apply transition to the newly created paths
     arcs.transition()
       .duration(500)
       .attrTween('d', arcTween);
   }
 
-  // Event handler for mouseover events
   function handleMouseOver(event, d) {
-    // Calculate the percentage of the total value for the current data point
     const percentage = ((d.data.value / d3.sum(data, (d) => d.value)) * 100).toFixed(2);
 
-    // Display information on screen using the tooltip
-    tooltip
-      .html(
-        `<strong>Eye Color:</strong> ${d.data.key}<br/>` +
-        `<strong>Amount:</strong> ${d.data.value}<br/>` +
-        `<strong>Percentage:</strong> ${percentage}%`
-      )
-      // Position the tooltip next to the mouse cursor
-      .style('left', event.pageX + 'px')
-      .style('top', event.pageY - 28 + 'px')
-      .style('opacity', 1);
+    tooltip.$set({ content: 
+      `<strong>Eye Color:</strong> ${d.data.key}<br/>` +
+      `<strong>Amount:</strong> ${d.data.value}<br/>` +
+      `<strong>Percentage:</strong> ${percentage}%`,
+      left: event.pageX + 'px',
+      top: event.pageY - 28 + 'px',
+      opacity: 1
+    });
   }
 
-  // Event handler for mouseout events
   function handleMouseOut() {
-    // Clear information and hide the tooltip on mouseout
-    tooltip.style('opacity', 0);
+    tooltip.$set({ opacity: 0 });
   }
 </script>
 
