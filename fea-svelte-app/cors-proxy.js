995e4eaee84daa89a2cf9025295bef7bf447bcca
Jarno Voogd <jarnovoogd@MacBook-Air.local>
2023-11-29 15:10:25 +0100

    installed package and added file to find a workaround for the CORS errors im getting

---
+++ fea-svelte-app/cors-proxy.js
@@ -0,0 +1,11 @@
+// const cors_proxy = require('cors-anywhere');
+
+// const host = 'localhost';
+// const port = 8080;
+
+// cors_proxy.createServer({
+//   originWhitelist: [], // Allow all origins
+//   requireHeaders: [], // Do not require any headers
+// }).listen(port, host, () => {
+//   console.log(`CORS Anywhere server is running on http://${host}:${port}`);
+// });
